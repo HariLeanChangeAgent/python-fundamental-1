e12caff23b67ab90d659e648c3cf2abc2c392951
HariLeanChangeAgent <hariatmi38@icloud.com>
2022-12-15 02:26:11 +0700

    percabangan

--- dasar_sintaksis_joke programmer.py
+++ dasar_sintaksis_joke programmer.py
@@ -1,9 +1,14 @@
 # percabangan
 jumlah_botol_susu = 2
-
+jumlah_telur = 1587
+print(f"jumlah botol susu {jumlah_botol_susu} btl")
+print(f"jumlah telur {jumlah_telur} butir")
 if jumlah_botol_susu > 0:
     print("budi mengecek harganya, dan ternyata uangnya cukup")
-    print("budi membeli satu botol susu")
+    if jumlah_telur == 0:
+        print("budi membeli satu botol susu")
+    else:
+        print("budi membeli 6 botol susu")
 else :
     print("budi tidak jadi membeli satu botol susu")
 

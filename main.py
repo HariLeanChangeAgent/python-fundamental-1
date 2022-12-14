4f99921001df54d77b5ca3f909fb1a9d700d1fdd
HariLeanChangeAgent <hariatmi38@icloud.com>
2022-12-14 14:24:36 +0700

    percabangan

---
+++ main.py
@@ -0,0 +1,11 @@
+# percabangan
+jumlah_botol_susu = 2
+
+if jumlah_botol_susu > 0:
+    print("budi mengecek harganya, dan ternyata uangnya cukup")
+    print("budi membeli satu botol susu")
+else :
+    print("budi tidak jadi membeli satu botol susu")
+
+print("budi pulang kerumah")
+print("menyerahkan hasilnya kepada ibu")
